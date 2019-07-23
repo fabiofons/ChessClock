3ff41d6363e9aacd9612f2866b8278dc9b828b92
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-07-23 14:16:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './clock.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const store = {
+  player: "",
+  playersFns: [],
+  subscribe(playerFn) {
+    this.playersFns.push(playerFn);
+  },
+  change() {
+    this.playersFns.forEach(fn => fn());
+  }
+};
+
+
+class Timers extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: 60,
+      // active: false
+    };
+
+    store.subscribe(() => {
+      if(store.player !== props.user){
+        this.interval = setInterval(() => {
+          this.setState(pastValue => ({
+            time: pastValue.time - 1,
+            // active: !this.state.active
+          }))
+        },1000)
+      }
+    })
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <div className={this.state.active ? 'clock active' : 'clock'} onClick={this.handleClick}>
+        <span className='time'>{this.state.time > 0 ? this.state.time : 'LOSE'}</span>
+      </div>
+    )
+  }
+
+  handleClick() {
+    store.player = this.props.user;
+    clearInterval(this.interval);
+    store.change();
+  }
 }
 
-export default App;
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Chess Clock Online</h1>
+        <div className='board'>        
+          <Timers user='player1' />
+          <Timers user='player2' />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
